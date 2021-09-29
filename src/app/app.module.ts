9c1c6cf5d54b08c2740c28370a91924d3d2a55af
p4ranoic0 <zeeke18@gmail.com>
2021-09-29 15:20:32 -0500

    Mejorado css y hipervinculos, diagrama de html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,25 @@ import { AppComponent } from './app.component';
 import { MainHeaderComponent } from './common/main-header/main-header.component';
 import { MainFooterComponent } from './common/main-footer/main-footer.component';
 import { MainNavComponent } from './common/main-nav/main-nav.component';
+import { BannerComponent } from './home/banner/banner.component';
+import { ExploreComponent } from './home/explore/explore.component';
+import { TrendingComponent } from './home/trending/trending.component';
+import { FeaturesComponent } from './home/features/features.component';
+import { TestimonialComponent } from './home/testimonial/testimonial.component';
+import { BlogsComponent } from './home/blogs/blogs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainHeaderComponent,
     MainFooterComponent,
-    MainNavComponent
+    MainNavComponent,
+    BannerComponent,
+    ExploreComponent,
+    TrendingComponent,
+    FeaturesComponent,
+    TestimonialComponent,
+    BlogsComponent
   ],
   imports: [
     BrowserModule,
